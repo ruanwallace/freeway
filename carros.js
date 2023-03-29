60a553310e90b9967fe4fc48c5c3e6fc1cce2704
ruanwallace <ruanwallace2k@gmail.com>
2023-03-29 18:44:52 -0300

    tirando os comentarios

--- carros.js
+++ carros.js
@@ -1,43 +1,33 @@
-// Variáveis com as imagens dos carros
 const carroVermelho = document.getElementById("carro1");
 const carroCinza = document.getElementById("carro2");
 const carroAzul = document.getElementById("carro3");
 
-// Lista dos carros
 const carros = [carroVermelho, carroCinza, carroAzul, carroCinza, carroVermelho, carroAzul];
 
-// Lista com as velocidades de cada carro
 const velocidades = [1.5, 1.7, 1.9, 1.6, 2.2, 1.8];
 
-// Posição dos carros
 let xCarros = [700, 700, 700, 700, 700, 700];
 const yCarros = [73, 122, 170, 224, 268, 317];
 
-// Tamanho dos carros
 const larguraCarro = 60;
 const alturaCarro = 39;
 
-// Posição Inicial dos carros
 const xInicialCarros = 700;
 
-// Áudio da colisão da galinha com os carros
 const audioColisao = document.getElementById("colisao");
 
-// Posiciona cada carro em sua respetiva posição
 function posicionaCarros() {
     for (let i = 0; i < carros.length; i++) {
         pincel.drawImage(carros[i], xCarros[i], yCarros[i], larguraCarro, alturaCarro);
     }
 }
 
-// Cria o loop dos carros, fazendo com que eles voltem para a posição inicial toda vez que percorrem toda a extensão da estrada
 function voltaOsCarrosParaXInicial(i) {
     if (xCarros[i] + larguraCarro < -20) {
         xCarros[i] = xInicialCarros;
     }
 }
 
-// Faz com que os carros se movam, decrementando a sua posição X
 function moveCarros() {
     for (let i = 0; i < carros.length; i++) {
         xCarros[i] -= velocidades[i];
@@ -46,7 +36,6 @@ function moveCarros() {
     }
 }
 
-// Esses são os parâmetros dos carros que definem as condições de colisão
 function parametroDosCarros(contador) {
     return yCarros[contador] + alturaCarro > yGalinha &&
     yCarros[contador] < yGalinha + alturaGalinha &&
@@ -54,7 +43,6 @@ function parametroDosCarros(contador) {
     xCarros[contador] < xGalinha + larguraGalinha;
 }
 
-// Faz com que haja a colisão da galinha com os carros
 function colisaoComAGalinha() {
     for (let i = 0; i < carros.length; i++) {
         if (parametroDosCarros(i)) {
--- freeway.js
+++ freeway.js
@@ -1,20 +1,14 @@
-// Relacionado com a pintura do fundo.
 const tela = document.querySelector("canvas");
 const pincel = tela.getContext("2d");
 
-// Imagem da estrada
 const estrada = document.getElementById("estrada");
 
-// Áudio da trilha sonora do jogo
 const audioTrilha = document.getElementById("trilha");
 
-// Botão para começar o jogo
 const buttonPlay = document.getElementById("play");
 
-// Mostra se o jogo está iniciado
 let inicializado = false;
 
-// Exibe a marcação de pontos na parte superior esquerda da tela
 function mostraPontos() {
     pincel.fillStyle = "black";
     pincel.font = "30px Arial";
@@ -23,13 +17,11 @@ function mostraPontos() {
     pincel.strokeText(meusPontos, 20, 66);
 }
 
-// Cria a tela
 function fundo() {
     pincel.drawImage(estrada, 0, 0);
     mostraPontos();
 }
 
-// Carrega as funções
 function atualiza() {
     fundo();
     posicionaCarros();
@@ -41,7 +33,6 @@ function atualiza() {
     marcaPonto();
 }
 
-// Associado a um botão, assim que é pressionado, o botão some e o jogo é inicializado
 function iniciaOJogo() {
     if (!inicializado) {
         setInterval(atualiza, 4);
@@ -51,8 +42,6 @@ function iniciaOJogo() {
     }
 }
 
-// Desenha a tela
 fundo();
 
-// Botão para iniciar o jogo
 buttonPlay.onclick = iniciaOJogo;
